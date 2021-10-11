ef9b3899b5aa0af3d0d9cd710960ca55d07d1685
AshisPatel <ashis.n.patel@gmail.com>
2021-10-11 14:30:43 -0500

    Add GET method to context provider

--- src/context/Context.js
+++ src/context/Context.js
@@ -1,11 +1,31 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 
 const { Provider, Consumer } = React.createContext();
 
 // Generally, the props are passed into the function, but we are destructuring the props object to grab the children key
 function ContextProvider({ children }) {
+    const [allPhotos, setAllPhotos] = useState(["photo1", "photo2"]);
+
+    async function fetchData() {
+        const response = await fetch('https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json',
+            {
+                method: "GET"
+            });
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+        } else {
+            alert("There was an issue retrieving the photos");
+        }
+
+    }
+
+    useEffect(() => {
+      fetchData();
+    }, []);
+
     return (
-        <Provider value="">
+        <Provider value={{ allPhotos }}>
             {children}
         </Provider>
     );
