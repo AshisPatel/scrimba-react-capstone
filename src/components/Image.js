1bce6e7bc79ba0355244493624da5d6dda74cceb
AshisPatel <ashis.n.patel@gmail.com>
2021-10-11 16:57:43 -0500

    Add state for isHovered in Image component

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Image({className, img}) {
+
+    const [isHovered, setisHovered] = useState(false);
+    
+    function hoverHandler(event){
+        setisHovered(prevState => !prevState.isHovered);
+    }
+
     return (
         <div className = {`${className} image-container`}>
-            <img src={img.url} className="image-grid"/>
+            <img src={img.url} className="image-grid" onMouseEnter={hoverHandler} onMoustLeave={hoverHandler}/>
         </div>
     )
 }
